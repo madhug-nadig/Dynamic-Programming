58c8b3694e48231da2580eb938f0298d490132ab
Madhu G Nadig <madhug.nadig@gmail.com>
2017-05-31 20:40:28 +0530

    Knapsack function init

--- Knapsack.py
+++ Knapsack.py
@@ -1,7 +1,22 @@
-value = [9,8,7,6,5]
+values = [9,8,7,6,5]
 weights = [1,2,3,4,5]
 
-def knap(p, k):
+def knap(w, v , k):
+    N = len(w)
+    table = [None] * N
+    for i in range(N):
+        table[i] = [None] * k
+    
+    for i in range(N):
+        table[i][0] = 0
+    
+    for i in range(N):
+        for j in range(1, k):
+            total_weight = j
+            if w[i] <= total_weight:
+                table[i][j] = w[i]
 
-print(p, len(p))
-print(knap(p, 10))
+    print(table)
+    return table[N-1][k-1]
+    
+print(knap(weights, values, 10))
