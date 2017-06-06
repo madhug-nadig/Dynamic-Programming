bc681d177749a6dfbc48723673d8a09900111542
Madhu G Nadig <madhug.nadig@gmail.com>
2017-06-06 06:08:36 +0530

    Finish up code

--- Non-DP Solutions/Coin Change - Recursive.py
+++ Non-DP Solutions/Coin Change - Recursive.py
@@ -1,5 +1,5 @@
-m = [1,2,3,4]
-n = 21
+S = [1,2,3]
+n = 5
 
 def count(S, m, n):
     if n == 0 :
@@ -9,5 +9,6 @@ def count(S, m, n):
     if m<=0 and n >=1:
         return 0
     
-    return count(S, m-1, n) + count(S, m, n-S[n-1])
-    
+    return count(S, m-1, n) + count(S, m, n-S[m-1])
+
+print(count(S, len(S), n))
